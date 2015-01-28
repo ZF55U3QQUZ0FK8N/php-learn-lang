393a12d5db9c439d275d15439df6736874ff8361
ZF55U3QQUZ0FK8N <ZF55U3QQUZ0FK8N@users.noreply.github.com>
2015-01-28 07:24:01 -0500

    Create index.php

---
+++ jp/index.php
@@ -0,0 +1,56 @@
+<html>
+<!--  head  -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <title>Learning Japanese</title>
+<!-- base href="http://www.grey-company.org/Circle/language/phrase.htm" -->
+<style>
+table, th, td {
+   /*
+ border: 0.01px solid black;
+   */
+border: 1px solid #000000;padding:3px; 
+
+}
+head {
+ cursor:url('http://24.media.tumblr.com/tumblr_mddt7tk96d1rkb6h0o1_r1_100.png'), auto 
+}
+html, body, {}
+a, a:hover {}    
+#scrollToTop:link, 
+#scrollToTop:visited {
+  display: none;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+}
+</style>
+</head> 
+<body alink="brown" bgcolor="white" link="green" vlink="green">
+<!--  head end -->
+<!--  title stuffs -->
+<center>
+<font size="+2">Learning Japanese</font><br>
+<hr/>
+<font size="-1"><font color="green" size="1"><i>shhhh... its a thing</i></font></font><br>
+<img src="img/line.gif" alt="? Ivy Divider ?" border="0"><br><br>
+</center>
+<?php 
+include ('./tags.php');
+?>
+<table width="94%"><tbody>
+<?php 
+include ('./output.php');
+?>
+<!--  data block -->
+    </tbody></table><br wp="br1">
+<!--  end ln block -->
+</br>
+<!--  FOOTER -->
+<br>
+<p></p>
+<center>
+<font size="-1">footer</font>
+</center>
+<p></p>
+</body></html>
