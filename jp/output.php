4c9ac508b2e32647f1867e19e90f59e24b66d50b
ZF55U3QQUZ0FK8N <ZF55U3QQUZ0FK8N@users.noreply.github.com>
2015-01-29 05:39:41 -0500

    Update output.php

--- jp/output.php
+++ jp/output.php
@@ -1,33 +1,77 @@
 <?php 
-$type = "Numbers";
+$type = "____";
 ##
 $myFile = "data.txt";
 $lines = file($myFile);//file in to an array
-#echo all # tags
-$hashtag="#";
+unset($myFile);
+$commentTag="#";
+
 #########
-foreach ($lines as &$value) {
-if(!(substr( $value, 0,  strlen($hashtag)  ) === "$hashtag"))
+
+echo"<table width=\"94%\">";
+echo"<tbody>";
+foreach ($lines as &$value) 
+{
+//del line brake
+$value = trim(preg_replace('/\s\s+/', ' ', $value));
+if(!(substr( $value, 0,  strlen($commentTag)  ) === "$commentTag")){
+
+
+if(!(substr( $value, 0,  strlen($titleTag)  ) === "$titleTag"))
+{
+if((isNotEmpty($value)))
 {
 $token = explode(':', $value);
 echo createDataBlock($token[0],$token[1],$token[2]);
-#echo "<hr/>";
-}else{
-echo createTitleBlock(substr( $value, 1,  strlen($value)));
-#echo "</ br>";
-#echo "<hr/>";
 }
+//echo "<hr/>";
 }
+else
+{
+createTitleBlock(substr( $value, 1,  strlen($value)));
+//echo "</ br>";
+//echo "<hr/>";
+}#if titleTag
+}#if commentTag
+}#foreach
+unset($type);
+unset($type);
+unset($type);
+
 unset($value);
-##
+echo"</tbody>";
+echo"</table>
+<br wp=\"br1\">";
+
+##function
 function createTitleBlock($Type) {
+
 $a = "33%";
 $VarA=$Type;
 $VarB="Romaji Translation";
 $VarC="Kanji Translation";
 
-return"<!--  title block -->".
-"<a name=\"$VarA\"></a>".
+echo "<!--  title block -->".
+"</tbody></table>";
+
+
+echo"<br wp=\"br1\">";
+ 
+echo"<br wp=\"br1\">";
+ 
+echo"<br wp=\"br1\">";
+ 
+include ('./tags.php');
+
+echo"<br wp=\"br1\">";
+ 
+echo"<br wp=\"br1\">";
+ 
+
+echo"<br wp=\"br1\">";
+ 
+echo "<a id=\"$VarA\"></a>".
+"<table width=\"94%\"><tbody>".
 "<tr>".
 "<td align=\"center\" valign=\"top\" width=\"$a\"><p></p><center><strong>$VarA</strong></center></td>".
 "<td align=\"center\" valign=\"top\" width=\"$a\"><p></p><center><strong>$VarB</strong></center></td>".
@@ -50,7 +94,19 @@ return"<!--  data block -->".
 
 }
 
+#
+function isNotEmpty($input) 
+{
+    $strTemp = $input;
+    $strTemp = trim($strTemp);
 
+    if($strTemp !== '') //Also tried this "if(strlen($strTemp) > 0)"
+    {
+         return true;
+    }
+
+    return false;
+}
 
 
 ?>
